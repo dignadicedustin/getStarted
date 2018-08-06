3e05670ff0cefcbf721b8059dcb163049781ccb7
Dustin Gil Isaac Dignadice <dignadicedustin261188@gmail.com>
2018-08-06 14:08:21 +1000

    Added reveal animation on services section

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+(function($) {
+    "use strict";
+    window.sr = ScrollReveal();
+    sr.reveal('.service-box', {
+      duration: 600,
+      scale: 0.3,
+      distance: '0px'
+    }, 200);
+
+})(jQuery);
